e3867c56bc9def998c5742b6c45d64f0946872c0
abhishek5012 <90345464+abhishek5012@users.noreply.github.com>
2022-01-30 06:00:33 -0800

    Update and rename project1 to project1.java

---
+++ project1.java
@@ -0,0 +1,12 @@
+package MyCaptain;
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//hello world programme
+		System.out.println("Hello World,");
+	}
+
+}
+
